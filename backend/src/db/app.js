ddb8656f95b3c9bc4d776a41517525c14db0065e
SOTA <sota00@outlook.jp>
2021-12-01 11:24:22 -0800

    trying to add search function

---
+++ backend/src/db/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+const port = 3001;
+
+mongoose.connect("mongodb://127.0.0.1:27017/best-dish-api");
+
+const app = express();
+
+const dishSchema = new mongoose.Schema({
+  dishName: String,
+  evaluation: Number,
+  price: Number,
+  location: String,
+  description: String,
+});
+
+const Dish = mongoose.model("Dish", dishSchema);
+
+router.get("/search", async (req, res) => {
+  const dishName = req.query.dishName;
+  const price = +req.query.price;
+
+  console.log(dishName, price);
+
+  try {
+    const dishes = await Dish.find({ dishName: dishName, price: price });
+    if (!dishes) return res.send("No menu matched");
+    console.log(dishes);
+    res.send(dishes);
+  } catch (e) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+app.use("/", router);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ backend/src/db/mongoose.js
@@ -0,0 +1,62 @@
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://127.0.0.1:27017/best-dish-api");
+
+// const Menu = mongoose.model("Menu", {
+//   dishName: {
+//     type: String,
+//   },
+//   evaluation: {
+//     type: Number,
+//   },
+//   price: {
+//     type: Number,
+//   },
+//   location: {
+//     type: String,
+//   },
+//   description: {
+//     type: String,
+//   },
+// });
+
+// const hamburger = new Menu({
+//   dishName: "hamburger",
+//   evaluation: 4,
+//   price: 10,
+//   location: "Texas",
+//   description: "This is good hamburger",
+// });
+// const pasta = new Menu({
+//   dishName: "pasta",
+//   evaluation: 3,
+//   price: 13,
+//   location: "Pali",
+//   description: "This is good pasta",
+// });
+// const tacos = new Menu({
+//   dishName: "tacos",
+//   evaluation: 3.5,
+//   price: 15,
+//   location: "Mexico city",
+//   description: "This is good tacos",
+// });
+// const ramen = new Menu({
+//   dishName: "ramen",
+//   evaluation: 4,
+//   price: 10,
+//   location: "Tokyo",
+//   description: "This is good ramen",
+// });
+// const curry = new Menu({
+//   dishName: "curry",
+//   evaluation: 4,
+//   price: 10,
+//   location: "delly",
+//   description: "This is good curry",
+// });
+
+// hamburger.save().then(() => console.log(hamburger));
+// pasta.save().then(() => console.log(pasta));
+// tacos.save().then(() => console.log(tacos));
+// ramen.save().then(() => console.log(ramen));
+// curry.save().then(() => console.log(curry));
