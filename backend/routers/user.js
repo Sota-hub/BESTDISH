b651b48c2a27d9334b760f9897344f54cf1c2e55
SOTA <sota00@outlook.jp>
2021-12-06 19:57:27 -0800

    add some variables

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -24,7 +24,7 @@ router.post("/users/signin", async (req, res) => {
   try {
     const user = await User.findByCredentials(email, password);
     const token = await user.generateToken();
-    res.send({ user: user.getPublicProfile(), token });
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -32,13 +32,16 @@ router.post("/users/signin", async (req, res) => {
 
 // Sign out
 router.post("/users/signout", auth, async (req, res) => {
-  try {
-    req.user.tokens = req.user.tokens.filter((token) => {
-      return token.token !== req.token;
-    });
-    await req.user.save();
+  const user = req.user;
+  const Token = req.token;
 
-    res.send(req.user);
+  try {
+    user.tokens = user.tokens.filter((token) => {
+      return token.token !== Token;
+    });
+    await user.save();
+
+    res.send(user);
   } catch (e) {
     res.status(500).send(e);
   }
@@ -46,11 +49,15 @@ router.post("/users/signout", auth, async (req, res) => {
 
 // Display authorized user profile
 router.get("/users/profile", auth, async (req, res) => {
-  res.send(req.user);
+  const user = req.user;
+  const token = req.token;
+
+  res.send({ user, token });
 });
 
 // Update user profile
-router.patch("/users/:id", auth, async (req, res) => {
+router.patch("/users/update", auth, async (req, res) => {
+  const user = req.user;
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password"];
   const isValidOperation = updates.every((update) =>
@@ -60,17 +67,8 @@ router.patch("/users/:id", auth, async (req, res) => {
   if (!isValidOperation) return res.status(400).send(e);
 
   try {
-    const _id = req.params.id;
-    const body = req.body;
-    const user = await User.findById(_id);
-
-    updates.forEach((update) => (user[update] = body[update]));
+    updates.forEach((update) => (user[update] = req.body[update]));
     await user.save();
-
-    if (!user) {
-      return res.status(404).send(e);
-    }
-
     res.send(user);
   } catch (e) {
     res.status(400).send(e);
